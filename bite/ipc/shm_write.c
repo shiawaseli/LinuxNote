9788446ea0405ed4cb1ab85fe377f4180c8a9454
shiawaseli <shiawaseli@users.noreply.github.com>
2019-12-10 23:55:13 +0800

    bite 其它进程通信方式、线程创建
    1、增加了进程间通信的笔记
    2、增加了共享内存实现进程间通信
    3、线程的创建

---
+++ bite/ipc/shm_write.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
+#define IPC_KEY 0x12345678
+#define PROJ_ID 0x12345678
+
+int main()
+{
+    //key_t key = ftok("./", PROJ_ID);
+    int shmid = shmget(IPC_KEY, 32, IPC_CREAT | 0664);
+    if (shmid < 0) {
+        perror("shmget error");
+        return -1;
+    }
+    void *shmstart = shmat(shmid, NULL, 0);
+    if (shmstart == (void*)-1) {
+        perror("shmat error");
+        return -1;
+    }
+    while (1) {
+        printf("%s", shmstart);
+        sleep(1);
+    }
+    shmdt(shmstart);
+    shmctl(shmid, IPC_RMID, NULL);
+    return 0;
+}
